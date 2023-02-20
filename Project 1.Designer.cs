a1dee5d63987ced9ffb8cad1aec6ac75bdd93474
BoxedLettuce <125930520+BoxedLettuce@users.noreply.github.com>
2023-02-20 18:04:37 -0500

    Add files via upload

---
+++ Project 1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Project_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Chiefs = new System.Windows.Forms.Label();
+            this.Eagles = new System.Windows.Forms.Label();
+            this.onePoint = new System.Windows.Forms.Button();
+            this.twoPoints = new System.Windows.Forms.Button();
+            this.threePoints = new System.Windows.Forms.Button();
+            this.sixPoints = new System.Windows.Forms.Button();
+            this.chiefsScore = new System.Windows.Forms.TextBox();
+            this.eaglesScore = new System.Windows.Forms.TextBox();
+            this.Commercial = new System.Windows.Forms.Button();
+            this.beerBreak = new System.Windows.Forms.Button();
+            this.beerPercent = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.commShown = new System.Windows.Forms.Label();
+            this.beerSales = new System.Windows.Forms.Label();
+            this.onePointE = new System.Windows.Forms.Button();
+            this.twoPointE = new System.Windows.Forms.Button();
+            this.threePointE = new System.Windows.Forms.Button();
+            this.sixPointE = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Chiefs
+            // 
+            this.Chiefs.AutoSize = true;
+            this.Chiefs.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Chiefs.Location = new System.Drawing.Point(12, 24);
+            this.Chiefs.Name = "Chiefs";
+            this.Chiefs.Size = new System.Drawing.Size(116, 38);
+            this.Chiefs.TabIndex = 0;
+            this.Chiefs.Text = "Chiefs";
+            this.Chiefs.Click += new System.EventHandler(this.Chiefs_Click);
+            // 
+            // Eagles
+            // 
+            this.Eagles.AutoSize = true;
+            this.Eagles.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Eagles.Location = new System.Drawing.Point(660, 24);
+            this.Eagles.Name = "Eagles";
+            this.Eagles.Size = new System.Drawing.Size(123, 38);
+            this.Eagles.TabIndex = 1;
+            this.Eagles.Text = "Eagles";
+            // 
+            // onePoint
+            // 
+            this.onePoint.Location = new System.Drawing.Point(12, 344);
+            this.onePoint.Name = "onePoint";
+            this.onePoint.Size = new System.Drawing.Size(75, 48);
+            this.onePoint.TabIndex = 2;
+            this.onePoint.Text = "1";
+            this.onePoint.UseVisualStyleBackColor = true;
+            this.onePoint.Click += new System.EventHandler(this.onePoint_Click);
+            // 
+            // twoPoints
+            // 
+            this.twoPoints.Location = new System.Drawing.Point(93, 344);
+            this.twoPoints.Name = "twoPoints";
+            this.twoPoints.Size = new System.Drawing.Size(75, 48);
+            this.twoPoints.TabIndex = 3;
+            this.twoPoints.Text = "2";
+            this.twoPoints.UseVisualStyleBackColor = true;
+            this.twoPoints.Click += new System.EventHandler(this.twoPoints_Click);
+            // 
+            // threePoints
+            // 
+            this.threePoints.Location = new System.Drawing.Point(174, 344);
+            this.threePoints.Name = "threePoints";
+            this.threePoints.Size = new System.Drawing.Size(75, 48);
+            this.threePoints.TabIndex = 4;
+            this.threePoints.Text = "3";
+            this.threePoints.UseVisualStyleBackColor = true;
+            this.threePoints.Click += new System.EventHandler(this.threePoints_Click);
+            // 
+            // sixPoints
+            // 
+            this.sixPoints.Location = new System.Drawing.Point(255, 344);
+            this.sixPoints.Name = "sixPoints";
+            this.sixPoints.Size = new System.Drawing.Size(75, 48);
+            this.sixPoints.TabIndex = 5;
+            this.sixPoints.Text = "6";
+            this.sixPoints.UseVisualStyleBackColor = true;
+            this.sixPoints.Click += new System.EventHandler(this.sixPoints_Click);
+            // 
+            // chiefsScore
+            // 
+            this.chiefsScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chiefsScore.Location = new System.Drawing.Point(19, 83);
+            this.chiefsScore.Name = "chiefsScore";
+            this.chiefsScore.Size = new System.Drawing.Size(100, 38);
+            this.chiefsScore.TabIndex = 6;
+            this.chiefsScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // eaglesScore
+            // 
+            this.eaglesScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eaglesScore.Location = new System.Drawing.Point(667, 83);
+            this.eaglesScore.Name = "eaglesScore";
+            this.eaglesScore.Size = new System.Drawing.Size(100, 38);
+            this.eaglesScore.TabIndex = 7;
+            this.eaglesScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Commercial
+            // 
+            this.Commercial.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Commercial.Location = new System.Drawing.Point(238, 96);
+            this.Commercial.Name = "Commercial";
+            this.Commercial.Size = new System.Drawing.Size(312, 77);
+            this.Commercial.TabIndex = 8;
+            this.Commercial.Text = "Commercial";
+            this.Commercial.UseVisualStyleBackColor = true;
+            this.Commercial.Click += new System.EventHandler(this.Commercial_Click);
+            // 
+            // beerBreak
+            // 
+            this.beerBreak.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beerBreak.Location = new System.Drawing.Point(238, 255);
+            this.beerBreak.Name = "beerBreak";
+            this.beerBreak.Size = new System.Drawing.Size(312, 58);
+            this.beerBreak.TabIndex = 9;
+            this.beerBreak.Text = "Beer Break";
+            this.beerBreak.UseVisualStyleBackColor = true;
+            this.beerBreak.Click += new System.EventHandler(this.beerBreak_Click);
+            // 
+            // beerPercent
+            // 
+            this.beerPercent.AutoSize = true;
+            this.beerPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beerPercent.Location = new System.Drawing.Point(235, 204);
+            this.beerPercent.Name = "beerPercent";
+            this.beerPercent.Size = new System.Drawing.Size(175, 16);
+            this.beerPercent.TabIndex = 10;
+            this.beerPercent.Text = "Thirsty percentage of crowd:";
+            this.beerPercent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(238, 223);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // commShown
+            // 
+            this.commShown.AutoSize = true;
+            this.commShown.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commShown.Location = new System.Drawing.Point(345, 46);
+            this.commShown.Name = "commShown";
+            this.commShown.Size = new System.Drawing.Size(0, 36);
+            this.commShown.TabIndex = 12;
+            // 
+            // beerSales
+            // 
+            this.beerSales.AutoSize = true;
+            this.beerSales.Location = new System.Drawing.Point(448, 223);
+            this.beerSales.Name = "beerSales";
+            this.beerSales.Size = new System.Drawing.Size(0, 16);
+            this.beerSales.TabIndex = 13;
+            // 
+            // onePointE
+            // 
+            this.onePointE.Location = new System.Drawing.Point(451, 344);
+            this.onePointE.Name = "onePointE";
+            this.onePointE.Size = new System.Drawing.Size(75, 48);
+            this.onePointE.TabIndex = 14;
+            this.onePointE.Text = "1";
+            this.onePointE.UseVisualStyleBackColor = true;
+            this.onePointE.Click += new System.EventHandler(this.onePointE_Click);
+            // 
+            // twoPointE
+            // 
+            this.twoPointE.Location = new System.Drawing.Point(532, 344);
+            this.twoPointE.Name = "twoPointE";
+            this.twoPointE.Size = new System.Drawing.Size(75, 48);
+            this.twoPointE.TabIndex = 15;
+            this.twoPointE.Text = "2";
+            this.twoPointE.UseVisualStyleBackColor = true;
+            this.twoPointE.Click += new System.EventHandler(this.twoPointE_Click);
+            // 
+            // threePointE
+            // 
+            this.threePointE.Location = new System.Drawing.Point(613, 344);
+            this.threePointE.Name = "threePointE";
+            this.threePointE.Size = new System.Drawing.Size(75, 48);
+            this.threePointE.TabIndex = 16;
+            this.threePointE.Text = "3";
+            this.threePointE.UseVisualStyleBackColor = true;
+            this.threePointE.Click += new System.EventHandler(this.threePointE_Click);
+            // 
+            // sixPointE
+            // 
+            this.sixPointE.Location = new System.Drawing.Point(694, 344);
+            this.sixPointE.Name = "sixPointE";
+            this.sixPointE.Size = new System.Drawing.Size(75, 48);
+            this.sixPointE.TabIndex = 17;
+            this.sixPointE.Text = "6";
+            this.sixPointE.UseVisualStyleBackColor = true;
+            this.sixPointE.Click += new System.EventHandler(this.sixPointE_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(298, 398);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(197, 40);
+            this.exit.TabIndex = 18;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.sixPointE);
+            this.Controls.Add(this.threePointE);
+            this.Controls.Add(this.twoPointE);
+            this.Controls.Add(this.onePointE);
+            this.Controls.Add(this.beerSales);
+            this.Controls.Add(this.commShown);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.beerPercent);
+            this.Controls.Add(this.beerBreak);
+            this.Controls.Add(this.Commercial);
+            this.Controls.Add(this.eaglesScore);
+            this.Controls.Add(this.chiefsScore);
+            this.Controls.Add(this.sixPoints);
+            this.Controls.Add(this.threePoints);
+            this.Controls.Add(this.twoPoints);
+            this.Controls.Add(this.onePoint);
+            this.Controls.Add(this.Eagles);
+            this.Controls.Add(this.Chiefs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Chiefs;
+        private System.Windows.Forms.Label Eagles;
+        private System.Windows.Forms.Button onePoint;
+        private System.Windows.Forms.Button twoPoints;
+        private System.Windows.Forms.Button threePoints;
+        private System.Windows.Forms.Button sixPoints;
+        private System.Windows.Forms.TextBox chiefsScore;
+        private System.Windows.Forms.TextBox eaglesScore;
+        private System.Windows.Forms.Button Commercial;
+        private System.Windows.Forms.Button beerBreak;
+        private System.Windows.Forms.Label beerPercent;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label commShown;
+        private System.Windows.Forms.Label beerSales;
+        private System.Windows.Forms.Button onePointE;
+        private System.Windows.Forms.Button twoPointE;
+        private System.Windows.Forms.Button threePointE;
+        private System.Windows.Forms.Button sixPointE;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ Project 1.cs
@@ -0,0 +1,116 @@
+﻿//Brandan Fennimore, Jacqueline Powers, Gurjot Athwal
+//Project 1
+//02/20/22
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1
+{
+    public partial class Form1 : Form
+    {
+        int eaglesScoreCounter;
+        int commercialCounter;
+        int chiefsScoreCounter;
+        int counterTwo;
+        int counterThree;
+        int counterSix;
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Chiefs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onePoint_Click(object sender, EventArgs e)
+        {
+            chiefsScoreCounter += 1;
+            chiefsScore.Text= chiefsScoreCounter.ToString();
+            
+        }
+
+        private void twoPoints_Click(object sender, EventArgs e)
+        {
+            chiefsScoreCounter+= 2;
+            chiefsScore.Text= chiefsScoreCounter.ToString();
+        }
+
+        private void Commercial_Click(object sender, EventArgs e)
+        {
+            commercialCounter += 600000000;
+            commShown.Text= commercialCounter.ToString();
+
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void beerBreak_Click(object sender, EventArgs e)
+        {
+            double percentage = 0;
+            Console.WriteLine("Enter a number: ");
+            percentage = double.Parse(Console.ReadLine());
+            Console.WriteLine(percentage.ToString());
+            Console.ReadLine();
+
+        }
+
+        private void threePoints_Click(object sender, EventArgs e)
+        {
+            chiefsScoreCounter += 3;
+            chiefsScore.Text = chiefsScoreCounter.ToString();
+        }
+
+        private void sixPoints_Click(object sender, EventArgs e)
+        {
+            chiefsScoreCounter += 6;
+            chiefsScore.Text = chiefsScoreCounter.ToString();
+        }
+
+        private void onePointE_Click(object sender, EventArgs e)
+        {
+            eaglesScoreCounter+= 1;
+            eaglesScore.Text= eaglesScoreCounter.ToString();
+        }
+
+        private void twoPointE_Click(object sender, EventArgs e)
+        {
+            eaglesScoreCounter += 2;
+            eaglesScore.Text = eaglesScoreCounter.ToString();
+        }
+
+        private void threePointE_Click(object sender, EventArgs e)
+        {
+            eaglesScoreCounter += 3;
+            eaglesScore.Text = eaglesScoreCounter.ToString();
+        }
+
+        private void sixPointE_Click(object sender, EventArgs e)
+        {
+            eaglesScoreCounter += 6;
+            eaglesScore.Text = eaglesScoreCounter.ToString();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
